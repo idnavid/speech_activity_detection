d2457c72fe9179c9832a2ff36d68a4cd25736f0c
nav <navid.shokouhi@utdallas.edu>
2015-10-27 15:38:07 -0500

    Modified segments to only include speech segments

--- openSAD2015/py_code/main_gmm_sad.py
+++ openSAD2015/py_code/main_gmm_sad.py
@@ -6,7 +6,7 @@ import os
 annotation_list = '../lists/all_sad_decoded.txt'
 
 fin = open(annotation_list)
-segments = """cat %s | cut -d '/' -f 6 | perl -ne 'if(m/(\S+)\\t(\S+)\\t(\S+)\\t(\S+)(.*)/){print "$2_$3_$4 $2 $3 $4\\n"}' >> %s"""
+segments = """cat %s | cut -d '/' -f 6 | perl -ne 'if(m/(\S+)\\t(\S+)\\t(\S+)\\t(\S+)\\t(S)\\t(.*)/){print "$2_$3_$4 $2 $3 $4\\n"}' >> %s"""
 
 fsegment = open('segments_command','w')
 for i in fin:
@@ -19,3 +19,4 @@ for i in fin:
     fsegment.write(segment_command+'\n')
  
 fsegment.close()
+
