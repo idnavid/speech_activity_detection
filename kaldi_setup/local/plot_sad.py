d2ecc232871ddb532aea2139d9b6fb96d34547c8
nav <navid.shokouhi@utdallas.edu>
2016-05-19 15:59:48 -0500

    added plot capabilities and parallel jobs submission.

---
+++ kaldi_setup/local/plot_sad.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python 
+
+import sys
+sys.path.append('/scratch/nxs113020/speech_activity_detection/unsupervised_sad')
+
+import audio_tools
+
+if __name__=='__main__':
+    """ system arguements:
+        1: wav filename
+        2: sad filename
+        3: sampling rate(optional)
+        4: frame length(optional)
+        5: frame shift(optional)
+        """
+    wav_fn = sys.argv[1]
+    vad_fn = sys.argv[2]
+    try:
+        fs = sys.argv[3]
+    except:
+        fs = 16000
+    try: 
+        winlen = sys.argv[4]
+    except:
+        winlen = (fs*0.025)
+    try: 
+        hoplen = sys.argv[5]
+    except:
+        hoplen = (fs*0.01)
+    mode = 'idx'
+    audio_tools.plot_vad(wav_fn, vad_fn, winlen, hoplen, mode)
+
--- unsupervised_sad/run_sad.sh
+++ unsupervised_sad/run_sad.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 wavlist=$1
-cfg_fn=./config_sad
+cfg_fn=$PWD/config_sad
 
 
-outdir=./sad_results_`basename ${wavlist%.*}`
+outdir=/erasable/nxs113020/sad_results_`basename ${wavlist%.*}`
 outformat="idx"
-mkdir $outdir
+mkdir -p $outdir
+
+if [ -f combosad_jobs.txt ]; then
+    rm combosad_jobs.txt
+fi
+touch combosad_jobs.txt
+
 for wav_fn in `cat $wavlist`; do
   stm_fn=$outdir/`basename ${wav_fn%.*}.stm`
-  echo "sad.py $wav_fn $cfg_fn $stm_fn $outformat"
-  python sad.py $wav_fn $cfg_fn $stm_fn $outformat
+  #echo "sad.py $wav_fn $cfg_fn $stm_fn $outformat"
+  echo "python $PWD/sad.py $wav_fn $cfg_fn $stm_fn $outformat" >> combosad_jobs.txt
 done
 
 
