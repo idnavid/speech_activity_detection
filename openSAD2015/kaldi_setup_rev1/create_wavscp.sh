0810f84c990bfd6aead2330833aa68e63892d42d
nav <navid.shokouhi@utdallas.edu>
2016-05-19 16:06:19 -0500

    updated openSAD files (second time)

--- openSAD2015/kaldi_setup_rev1/create_wavscp.sh
+++ openSAD2015/kaldi_setup_rev1/create_wavscp.sh
@@ -3,3 +3,8 @@
 for channel in B D E F G H; do
 ls /scratch2/share/nxs113020/SAD-2015-Challenge-Data/LDC2015E96_NIST_OpenSAD15_Training/data/train/wav/*/*/*.wav | grep "_$channel.wav" | perl -ne 'if(m/\S+\/(\S+).wav/){print "$1 $_"}' | sort  > data/train_$channel/wav.scp
 done;
+
+
+for channel in A B C D E F G H XA XH XI XK XMT XN; do
+    ls /scratch2/share/nxs113020/SAD-2015-Challenge-Data/LDC2015E98_NIST_OpenSAD15_Eval/data/progress/audio/*/*.wav | grep "_$channel.wav" | perl -ne 'if(m/\S+\/(\S+).wav/){print "$1 $_"}' | sort  > data/train_$channel/wav.scp
+done;
